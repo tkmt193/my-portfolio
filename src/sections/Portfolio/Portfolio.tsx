5dc0840f9924e3e44147f9599d9c24579a336f46
塚本育美 <tsukamotoikumi@tsukamotoikuminoMacBook-Pro.local>
2026-02-26 15:00:09 +0900

    add my works infomation

--- src/sections/Portfolio/Portfolio.tsx
+++ src/sections/Portfolio/Portfolio.tsx
@@ -1,16 +1,29 @@
 import styles from "./Portfolio.module.css";
 import * as motion from "motion/react-client";
+import portfolio from "../../assets/portfolio.png";
+import comming from "../../assets/comming.png";
+import work from "../../assets/work.png";
 
 interface CardProps {
+	link: string;
 	category: string;
 	title: string;
 	description: string;
-	imageText: string;
+	image: string;
 }
 
-function PortfolioCard({ category, title, description, imageText }: CardProps) {
+function PortfolioCard({
+	link,
+	category,
+	title,
+	description,
+	image,
+}: CardProps) {
 	return (
-		<motion.div
+		<motion.a
+			href={link}
+			target="_blank"
+			rel="noopener noreferrer"
 			className={styles.card}
 			whileHover={{
 				scale: 1.1,
@@ -18,14 +31,14 @@ function PortfolioCard({ category, title, description, imageText }: CardProps) {
 			whileTap={{ scale: 0.95 }}
 		>
 			<div className={styles.cardImg}>
-				<span>{imageText}</span>
+				<img src={image} alt={title} className={styles.responsiveImg} />
 			</div>
 			<div className={styles.cardBody}>
 				<span className={styles.cardCategory}>{category}</span>
 				<h3 className={styles.cardTitle}>{title}</h3>
 				<p className={styles.cardDescription}>{description}</p>
 			</div>
-		</motion.div>
+		</motion.a>
 	);
 }
 
@@ -35,35 +48,42 @@ const works = [
 		category: "WEB",
 		title: "My Portfolio",
 		description: "A web portfolio built with React",
-		imageText: "Concept Design",
+		image: portfolio,
+		link: "https://ikumi-portfolio.netlify.app/",
 	},
 	{
 		id: "2",
-		category: "WEB",
-		title: "Coming Soon...",
-		description: "I am currently working on a new project.",
-		imageText: "Web Development",
+		category: "Resume",
+		title: "My Professional Experience",
+		description: "Detailed Work History",
+		image: work,
+		link: "https://peppered-cap-e3a.notion.site/My-Professional-Experience-13ad9460212f80cb9915cefc72bc2919?pvs=143",
 	},
 	{
 		id: "3",
-		category: "WEB",
-		title: "Coming Soon...",
-		description: "I am currently working on a new project.",
-		imageText: "Web Development",
+		category: "Blog",
+		title: "Zenn | Tech Blog",
+		description:
+			"I also share content on several other technology-related topics.",
+		image:
+			"https://res.cloudinary.com/zenn/image/upload/s--KwG6M5YQ--/c_fit%2Cg_north_west%2Cl_text:notosansjp-medium.otf_55:%25E3%2580%2590%25E5%25AE%259F%25E8%25B7%25B5%25E3%2582%25AC%25E3%2582%25A4%25E3%2583%2589%25E3%2580%2591AWS%2520Lambda%2520%252B%2520Docker%2520%252B%2520DuckDB%25E3%2581%25A7%25E4%25BD%259C%25E3%2582%258B%25E3%2583%2587%25E3%2583%25BC%25E3%2582%25BF%25E5%2587%25A6%25E7%2590%2586%25E5%259F%25BA%25E7%259B%25A4%2520-%2520Python%25203....%2Cw_1010%2Cx_90%2Cy_100/g_south_west%2Cl_text:notosansjp-medium.otf_37:%25E3%2581%25BF%2Cx_203%2Cy_121/g_south_west%2Ch_90%2Cl_fetch:aHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL3plbm4tdXNlci11cGxvYWQvYXZhdGFyL2NiZjcyMjc2NTQuanBlZw==%2Cr_max%2Cw_90%2Cx_87%2Cy_95/v1627283836/default/og-base-w1200-v2.png?_a=BACAGSGT); background-color:unset;",
+		link: "https://zenn.dev/mimimi193/articles/20250521-lambda-duckdb",
 	},
 	{
 		id: "4",
 		category: "WEB",
 		title: "Coming Soon...",
 		description: "I am currently working on a new project.",
-		imageText: "Web Development",
+		image: comming,
+		link: "https://ikumi-portfolio.netlify.app/",
 	},
 	{
 		id: "5",
 		category: "WEB",
 		title: "Coming Soon...",
 		description: "I am currently working on a new project.",
-		imageText: "Web Development",
+		image: comming,
+		link: "https://ikumi-portfolio.netlify.app/",
 	},
 ];
 
@@ -75,7 +95,7 @@ export default function Portfolio({
 	return (
 		<section ref={ref} className={styles.portfolioContainer}>
 			<div className={styles.sectionHeader}>
-				<h2>Selected Works</h2>
+				<h2>My Works</h2>
 			</div>
 
 			<div className={styles.tickerWrapper}>
